a1958e9381cb7c7cf30a514d3db13b20b84bb8ff
Ofir Tene <63402158+ofirte@users.noreply.github.com>
2020-10-04 18:35:04 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
-const mongoose=require('mongoose');
 var bodyParser = require('body-parser') 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
